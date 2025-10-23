2543c903c66afef9d94e76d025dadf1772394001
whatdouallwant <masya.babenko@gmail.com>
2025-10-23 18:19:31 +0300

    added edit system

--- journalapp/urls.py
+++ journalapp/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import include, path
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('edit/', views.edit_journal, name='edit_journal'),
 ]
--- journalapp/views.py
+++ journalapp/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 from journalapp.models import JournalStudents, Lesson
 
 def home(request):
     lessons = Lesson.objects.all()
     journal_students = JournalStudents.objects.all()
     return render(request, 'journal/journal.html', {'journal_students': journal_students, 'lessons': lessons})
+
+@login_required
+def edit_journal(request):
+    lessons = Lesson.objects.all()
+    journal_students = JournalStudents.objects.all()
+    return render(request, 'journal/edit_journal.html', {'journal_students': journal_students, 'lessons': lessons})
+
 
 
