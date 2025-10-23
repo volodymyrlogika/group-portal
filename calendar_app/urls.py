90f43356ca91d43f8e579b93c6b26bc97200d9ee
BruhWaxie <efirestmax@gmail.com>
2025-10-23 18:41:18 +0300

    --Fixed urls.py problem

--- calendar_app/urls.py
+++ calendar_app/urls.py
@@ -3,5 +3,5 @@ from . import views
 from .views import calendar_view
 
 urlpatterns = [
-    path('calendar/', calendar_view, name='calendar'),
+    path('', calendar_view, name='calendar'),
 ] 
--- group_portal/urls.py
+++ group_portal/urls.py
@@ -22,7 +22,6 @@ from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls')),
     path('', include('accounts.urls')),
     path('calendar/', include('calendar_app.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
