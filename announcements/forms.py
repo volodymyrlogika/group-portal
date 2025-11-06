de17f26f9248897f4ead01ce985087ff976578e5
pavlofe <fedakpavlo70@gmail.com>
2025-11-06 16:40:10 +0200

    .

---
+++ announcements/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Announcements
+
+class AnnouncementsForm(forms.ModelForm):
+    class Meta:
+        model = Announcements
+        fields = ['title', 'content ', 'attachment', 'status']
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({'class': 'form-control mb-2', })
--- announcements/urls.py
+++ announcements/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.announcements_list, name="announcements_list"),
+    path('', views.announcements_list, name='announcements_list'),
 ]
