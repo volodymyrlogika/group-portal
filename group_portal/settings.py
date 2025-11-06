e2579cedcc25f041f5e1990bed6027ad11aa5f34
volodymyr <volodymyrlogika@gmail.com>
2025-11-06 18:19:35 +0200

    refactor: update STATIC_ROOT path and improve user model import

--- group_portal/settings.py
+++ group_portal/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Media files
 MEDIA_URL = '/media/'
